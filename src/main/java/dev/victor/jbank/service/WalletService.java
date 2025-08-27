46f23d61e9d4eddf4aa7f140564fe11aa481f68f
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 09:29:38 -0300

    feat: Implement depositMoney with transaction, balance update, and audit data

--- src/main/java/dev/victor/jbank/service/WalletService.java
+++ src/main/java/dev/victor/jbank/service/WalletService.java
@@ -1,13 +1,20 @@
 package dev.victor.jbank.service;
 
 import dev.victor.jbank.controller.dto.CreateWalletDto;
+import dev.victor.jbank.controller.dto.DepositMoneyDto;
+import dev.victor.jbank.entity.Deposit;
 import dev.victor.jbank.entity.Wallet;
 import dev.victor.jbank.exception.DeleteWalletException;
 import dev.victor.jbank.exception.WalletDataAlreadyExistsException;
+import dev.victor.jbank.exception.WalletNotFoundException;
+import dev.victor.jbank.repository.DepositRepository;
 import dev.victor.jbank.repository.WalletRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,9 +22,11 @@ import java.util.UUID;
 public class WalletService {
 
     private final WalletRepository walletRepository;
+    private final DepositRepository depositRepository;
 
-    public WalletService(WalletRepository walletRepository) {
+    public WalletService(WalletRepository walletRepository, DepositRepository depositRepository) {
         this.walletRepository = walletRepository;
+        this.depositRepository = depositRepository;
     }
 
 
@@ -47,5 +56,21 @@ public class WalletService {
             walletRepository.deleteById(walletId);
         }
         return wallet.isPresent();
+    }
+
+    @Transactional
+    public void depositMoney(UUID walletId, DepositMoneyDto dto, String ipAddress) {
+        Wallet wallet = walletRepository.findById(walletId)
+                .orElseThrow(() -> new WalletNotFoundException("There is no wallet with id"));
+
+        Deposit deposit = new Deposit();
+        deposit.setWallet(wallet);
+        deposit.setDepositValue(dto.value());
+        deposit.setDepositDateTime(OffsetDateTime.now());
+        deposit.setIpAddress(ipAddress);
+
+        depositRepository.save(deposit);
+        wallet.setBalance(wallet.getBalance().add(dto.value()));
+        walletRepository.save(wallet);
     }
 }
