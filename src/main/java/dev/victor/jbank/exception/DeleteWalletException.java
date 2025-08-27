0b294373e0c99b0fea56e4789e9f93fa731fec7d
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 02:56:12 -0300

    feat: Add DeleteWalletException with 422 ProblemDetail response

---
+++ src/main/java/dev/victor/jbank/exception/DeleteWalletException.java
@@ -0,0 +1,22 @@
+package dev.victor.jbank.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class DeleteWalletException extends JBankException {
+
+    private final String detail;
+
+    public DeleteWalletException(String detail) {
+        super(detail);
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+        pd.setTitle("You cannot delete this wallet");
+        pd.setDetail(detail);
+        return pd;
+    }
+}
