c834caf74053641115e96212b3ebbc93cbe0d7f3
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 02:16:22 -0300

    feat: Customize WalletDataAlreadyExistsException to return 422 ProblemDetail

--- src/main/java/dev/victor/jbank/exception/WalletDataAlreadyExistsException.java
+++ src/main/java/dev/victor/jbank/exception/WalletDataAlreadyExistsException.java
@@ -1,8 +1,23 @@
 package dev.victor.jbank.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
 public class WalletDataAlreadyExistsException extends JBankException {
 
-    public WalletDataAlreadyExistsException(String message) {
-        super(message);
+    private final String detail;
+
+    public WalletDataAlreadyExistsException(String detail) {
+        super(detail);
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        pd.setTitle("Wallet data already exists");
+        pd.setDetail(detail);
+        return pd;
     }
 }
