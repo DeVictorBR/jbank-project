3e7e43e31f88cd32ac1226005d80ff73af3fa561
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 12:51:34 -0300

    refactor: Register multiple filters with specific order

--- src/main/java/dev/victor/jbank/filter/FilterConfig.java
+++ src/main/java/dev/victor/jbank/filter/FilterConfig.java
@@ -8,15 +8,25 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
     private final IpFilter ipFilter;
+    private final PerformanceFilter performanceFilter;
 
-    public FilterConfig(IpFilter ipFilter) {
+    public FilterConfig(IpFilter ipFilter, PerformanceFilter performanceFilter) {
         this.ipFilter = ipFilter;
+        this.performanceFilter = performanceFilter;
     }
 
     @Bean
-    public FilterRegistrationBean<IpFilter> filterFilterRegistrationBean() {
+    public FilterRegistrationBean<IpFilter> ipFilterRegistrationBean() {
         FilterRegistrationBean<IpFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(ipFilter);
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<PerformanceFilter> performanceFilterRegistration() {
+        FilterRegistrationBean<PerformanceFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(performanceFilter);
         registrationBean.setOrder(0);
         return registrationBean;
     }
