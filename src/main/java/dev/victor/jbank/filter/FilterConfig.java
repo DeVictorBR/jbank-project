ce526d487ee061910699cab3a9ebcff41e25a2c5
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-26 22:27:36 -0300

    feat: Register IpFilter with highest precedence in FilterConfig

---
+++ src/main/java/dev/victor/jbank/filter/FilterConfig.java
@@ -0,0 +1,23 @@
+package dev.victor.jbank.filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    private final IpFilter ipFilter;
+
+    public FilterConfig(IpFilter ipFilter) {
+        this.ipFilter = ipFilter;
+    }
+
+    @Bean
+    public FilterRegistrationBean<IpFilter> filterFilterRegistrationBean() {
+        FilterRegistrationBean<IpFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(ipFilter);
+        registrationBean.setOrder(0);
+        return registrationBean;
+    }
+}
