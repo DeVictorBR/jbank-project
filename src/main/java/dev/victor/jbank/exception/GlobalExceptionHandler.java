fb66be809e182114a2d8ecc11cd512cf0417b87c
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 02:37:09 -0300

    feat: Handle MethodArgumentNotValidException with ProblemDetail including invalid params

--- src/main/java/dev/victor/jbank/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/victor/jbank/exception/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package dev.victor.jbank.exception;
 
+import dev.victor.jbank.exception.dto.InvalidParamDto;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,5 +14,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JBankException.class)
     public ProblemDetail handleJBankException(JBankException e) {
         return e.toProblemDetail();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<InvalidParamDto> invalidParams = e.getFieldErrors()
+                .stream()
+                .map(fe -> new InvalidParamDto(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+        ProblemDetail pd = ProblemDetail.forStatus(400);
+        pd.setTitle("Invalid request parameters");
+        pd.setDetail("There is invalid fields on the request");
+        pd.setProperty("invalid params", invalidParams);
+
+        return pd;
     }
 }
