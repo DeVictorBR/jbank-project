aca58883a5b554b5381684e8f94f1af28cc6b4c9
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 02:57:04 -0300

    feat: Add deleteWallet method with balance check and DeleteWalletException

--- src/main/java/dev/victor/jbank/service/WalletService.java
+++ src/main/java/dev/victor/jbank/service/WalletService.java
@@ -2,12 +2,14 @@ package dev.victor.jbank.service;
 
 import dev.victor.jbank.controller.dto.CreateWalletDto;
 import dev.victor.jbank.entity.Wallet;
+import dev.victor.jbank.exception.DeleteWalletException;
 import dev.victor.jbank.exception.WalletDataAlreadyExistsException;
 import dev.victor.jbank.repository.WalletRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class WalletService {
@@ -34,5 +36,16 @@ public class WalletService {
         newWallet.setEmail(dto.email());
 
         return walletRepository.save(newWallet);
+    }
+
+    public boolean deleteWallet(UUID walletId) {
+        Optional<Wallet> wallet = walletRepository.findById(walletId);
+        if (wallet.isPresent()) {
+            if (wallet.get().getBalance().compareTo(BigDecimal.ZERO) != 0) {
+                throw new DeleteWalletException("The balance is not zero");
+            }
+            walletRepository.deleteById(walletId);
+        }
+        return wallet.isPresent();
     }
 }
