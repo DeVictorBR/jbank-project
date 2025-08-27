c73c84b17f935466d48b2b55bfb00fb944bbe719
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 10:45:25 -0300

    feat: Add TransferController with POST /transfers endpoint

---
+++ src/main/java/dev/victor/jbank/controller/TransferController.java
@@ -0,0 +1,27 @@
+package dev.victor.jbank.controller;
+
+import dev.victor.jbank.controller.dto.TransferMoneyDto;
+import dev.victor.jbank.service.TransferService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/transfers")
+public class TransferController {
+
+    private final TransferService transferService;
+
+    public TransferController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> transfer(@RequestBody @Valid TransferMoneyDto dto) {
+        transferService.transferMoney(dto);
+        return ResponseEntity.ok().build();
+    }
+}
