bcf2585d190c99931943964f4292df158c138d41
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 09:29:08 -0300

    feat: Add WalletNotFoundException with ProblemDetail support

---
+++ src/main/java/dev/victor/jbank/exception/WalletNotFoundException.java
@@ -0,0 +1,22 @@
+package dev.victor.jbank.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class WalletNotFoundException extends JBankException {
+
+    private final String detail;
+
+    public WalletNotFoundException(String detail) {
+        super(detail);
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        ProblemDetail pd = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        pd.setTitle("Wallet not found");
+        pd.setDetail(detail);
+        return pd;
+    }
+}
