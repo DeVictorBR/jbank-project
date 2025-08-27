a3168a6725774717614c5d68d4c4dddb08ed14b3
victor barbosa <victorhenriquebl11@gmail.com>
2025-08-27 02:14:50 -0300

    feat: Add toProblemDetail method to JBankException for standardized error responses

--- src/main/java/dev/victor/jbank/exception/JBankException.java
+++ src/main/java/dev/victor/jbank/exception/JBankException.java
@@ -1,5 +1,7 @@
 package dev.victor.jbank.exception;
 
+import org.springframework.http.ProblemDetail;
+
 public abstract class JBankException extends RuntimeException {
 
     public JBankException(String message) {
@@ -8,5 +10,12 @@ public abstract class JBankException extends RuntimeException {
 
     public JBankException(Throwable cause) {
         super(cause);
+    }
+
+    public ProblemDetail toProblemDetail() {
+        ProblemDetail pd = ProblemDetail.forStatus(500);
+        pd.setTitle("JBank Internal Server Error");
+        pd.setDetail("Contact JBank Support");
+        return pd;
     }
 }
